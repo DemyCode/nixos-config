c2e3d74c915b3521be02175f4cd8f64127b3c891
DemyCode <verycols@gmail.com>
2025-01-15 22:39:17 +0100

    hyprland cachix

--- configuration.nix
+++ configuration.nix
@@ -138,5 +138,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
