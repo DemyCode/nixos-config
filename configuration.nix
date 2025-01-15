79acda9d315ab1994e5cd79968e7392e9bbbfe4e
DemyCode <verycols@gmail.com>
2025-01-15 23:00:35 +0100

    fix: removing gnome

--- configuration.nix
+++ configuration.nix
@@ -44,17 +44,17 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "fr";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+  #   layout = "fr";
+  #   variant = "";
+  # };
 
   # Configure console keymap
   console.keyMap = "fr";
