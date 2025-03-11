83cf26aacd305f303e1da81d35833182d502900a
DemyCode <verycols@gmail.com>
2025-03-11 21:45:18 +0100

    fix: neovim

--- configuration.nix
+++ configuration.nix
@@ -97,7 +97,20 @@
     librewolf
     uv
     python3
+    clang
   ];
+
+  fonts.packages = with pkgs; [
+  noto-fonts
+  noto-fonts-cjk-sans
+  noto-fonts-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+];
 
   programs.nix-ld.enable = true;
 
