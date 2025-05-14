470a3769804dc9868301dd5962ede46c5c9e9619
DemyCode <verycols@gmail.com>
2025-05-14 18:22:09 +0200

    fix: lets go

--- home.nix
+++ home.nix
@@ -49,6 +49,31 @@
     })
     docker
     awscli2
+    (python312.withPackages (ps:
+      with ps; [
+        pynvim
+        jupyter
+        cairosvg
+        pnglatex
+        plotly
+        kaleido
+        pyperclip
+        nbformat
+        pillow
+        jupyter
+        ipython
+        pip
+        (buildPythonPackage rec {
+          pname = "jupyter_client";
+          version = "8.6.3";
+          format = "wheel";
+          src = fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-NbOglHxKbp1Ynrl9fUzV6Q+RDucxAWEfASg3Mr1tlBk=";
+          };
+          doCheck = false;
+        })
+      ]))
   ];
 
   home.pointerCursor = let
@@ -93,5 +118,23 @@
   home.sessionVariables = { EDITOR = "nvim"; };
 
   programs.home-manager.enable = true;
-  programs.neovim = { enable = true; };
+  programs.neovim = {
+    enable = true;
+    extraPython3Packages = ps:
+      with ps; [
+        # ... other python packages
+        pynvim
+        jupyter-client
+        cairosvg # for image rendering
+        pnglatex # for image rendering
+        plotly # for image rendering
+        pyperclip
+        nbformat
+      ];
+    extraPackages = with pkgs;
+      [
+        # ... other packages
+        imagemagick # for image rendering
+      ];
+  };
 }
--- starter/lua/plugins/jupytext.lua
+++ starter/lua/plugins/jupytext.lua
@@ -1,6 +1,46 @@
 return {
-  "GCBallesteros/jupytext.nvim",
-  config = true,
-  -- Depending on your nvim distro or config you may need to make the loading not lazy
-  -- lazy=false,
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- this is an example, not a default. Please see the readme for more configuration options
+      vim.g.molten_output_win_max_height = 12
+      -- I find auto open annoying, keep in mind setting this option will require setting
+      -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
+      vim.g.molten_auto_open_output = false
+
+      -- this guide will be using image.nvim
+      -- Don't forget to setup and install the plugin if you want to view image outputs
+      vim.g.molten_image_provider = "image.nvim"
+
+      -- optional, I like wrapping. works for virt text and the output window
+      vim.g.molten_wrap_output = true
+
+      -- Output as virtual text. Allows outputs to always be shown, works with images, but can
+      -- be buggy with longer images
+      vim.g.molten_virt_text_output = true
+
+      -- this will make it so the output shows up below the \`\`\` cell delimiter
+      vim.g.molten_virt_lines_off_by_1 = true
+    end,
+    keys = {
+      { "<localleader>e", "<cmd>MoltenEvaluateOperator<CR>", desc = "evaluate operator" },
+      { "<localleader>os", "<cmd>noautocmd MoltenEnterOutput<CR>", desc = "open output window" },
+      { "<localleader>oh", "<cmd>MoltenHideOutput<CR>", desc = "close output window" },
+      { "<localleader>rr", "<cmd>MoltenReevaluateCell<CR>", desc = "re-eval cell" },
+      { "<localleader>r", "<cmd>MoltenEvaluateVisual<CR>", desc = "execute visual selection" },
+      { "<localleader>md", "<cmd>MoltenDelete<CR>", desc = "delete Molten cell" },
+      { "<localleader>mx", "<cmd>MoltenOpenInBrowser<CR>", desc = "open output in browser" },
+    },
+    dependencies = {
+      { "3rd/image.nvim" },
+    },
+    lazy = false,
+  },
+  {
+    "GCBallesteros/jupytext.nvim",
+    lazy = false,
+    config = true,
+  },
 }
