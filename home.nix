a175168e76e6da8025845f0818e664742b1a2e15
DemyCode <verycols@gmail.com>
2025-06-16 15:17:23 +0000

    fix: removing zsh

--- home.nix
+++ home.nix
@@ -77,17 +77,6 @@
       ]))
   ];
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
-    };
-    history.size = 10000;
-  };
   programs.nix-index = {
     enable = true;
     enableFishIntegration = true;
