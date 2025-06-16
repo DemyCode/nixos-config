ea8b45336d90e26107509a034f82fba024030346
DemyCode <verycols@gmail.com>
2025-06-16 21:07:43 +0000

    fix: lets go

---
+++ configuration-msi.nix
@@ -0,0 +1,79 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, inputs, ... }: {
+  imports = [ ./hardware-configuration-msi.nix ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # Wifi Card and ethernet 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs;
+      [
+        #  thunderbird
+      ];
+  };
+  programs.nix-ld.enable = true;
+  programs.steam.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  # Nvidia setup
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+  hardware.opengl.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+
+  nix.settings.experimental-features = "nix-command flakes";
+  environment.systemPackages = with pkgs; [ spotify ];
+  networking.firewall.allowedTCPPorts = [ 57621 ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
--- flake.nix
+++ flake.nix
@@ -26,6 +26,18 @@
           ];
           specialArgs = { inherit inputs; };
         };
+        msi = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./configuration-msi.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nixos = ./home.nix;
+            }
+          ];
+          specialArgs = { inherit inputs; };
+        };
         wsl = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
