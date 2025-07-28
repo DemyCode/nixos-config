f72ecb6259afcf59205267a731d4e161ebad3516
DemyCode <verycols@gmail.com>
2025-07-28 21:57:55 +0200

    fix: old method

--- flake.nix
+++ flake.nix
@@ -10,71 +10,102 @@
   inputs.nix-software-center.url = "github:snowfallorg/nix-software-center";
 
   outputs =
-    { self, nixpkgs, home-manager, nixos-wsl, nix-index-database, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-wsl,
+      nix-index-database,
+      ...
+    }@inputs:
     let
-      deepMerge = lhs: rhs:
+      deepMerge =
+        lhs: rhs:
         if builtins.typeOf lhs != builtins.typeOf rhs then
           lhs
         else if builtins.isList lhs then
           lhs ++ rhs
         else if builtins.isAttrs lhs then
-          lhs // rhs // builtins.mapAttrs (name: value:
-            if builtins.hasAttr name rhs then
-              deepMerge value (rhs.${name})
-            else
-              value) lhs
+          lhs
+          // rhs
+          // builtins.mapAttrs (
+            name: value: if builtins.hasAttr name rhs then deepMerge value (rhs.${name}) else value
+          ) lhs
         else
-          rhs;
-    in deepMerge (let
-      nixos-func = modules:
-        (nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = modules;
-          specialArgs = { inherit inputs; };
-        });
-      home-manager-func = home-manager-module: [
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nixos = home-manager-module;
-        }
-        nix-index-database.nixosModules.nix-index
-      ];
-      # Use this for all other targets
-      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
-    in {
-      nixosConfigurations = {
-        asus = nixos-func ([ ./configuration-asus.nix ]
-          ++ home-manager-func ./home-desktop.nix);
-        msi = nixos-func
-          ([ ./configuration-msi.nix ] ++ home-manager-func ./home-desktop.nix);
-        wsl = nixos-func
-          ([ ./configuration-wsl.nix nixos-wsl.nixosModules.default ]
-            ++ home-manager-func ./home-wsl.nix);
-      };
-      homeConfigurations = {
-        "default" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          modules = [ ./home-wsl.nix ];
-        };
-      };
-    }) ({
-      nixosConfigurations = {
-        dada = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ({ config, pkgs, lib, inputs, ... }: {
+          lhs;
+      conf = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (
+            {
+              config,
+              pkgs,
+              lib,
+              inputs,
+              ...
+            }:
+            {
               nixpkgs.config.check = false;
               fileSystems."/" = {
-                device =
-                  "/dev/disk/by-uuid/88a22c74-e2d3-4e02-ab95-5211a3e407eb";
+                device = "/dev/disk/by-uuid/88a22c74-e2d3-4e02-ab95-5211a3e407eb";
                 fsType = "ext4";
               };
               boot.loader.systemd-boot.enable = true;
-            })
-          ];
-        };
+              environment.systemPackages = with pkgs; [
+                cargo
+              ];
+            }
+          )
+        ];
       };
-    });
+    in
+    deepMerge
+      (
+        let
+          nixos-func =
+            modules:
+            (nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = modules;
+              specialArgs = { inherit inputs; };
+            });
+          home-manager-func = home-manager-module: [
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nixos = home-manager-module;
+            }
+            nix-index-database.nixosModules.nix-index
+          ];
+          # Use this for all other targets
+          # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
+        in
+        {
+          nixosConfigurations = {
+            asus = nixos-func ([ ./configuration-asus.nix ] ++ home-manager-func ./home-desktop.nix);
+            msi = nixos-func ([ ./configuration-msi.nix ] ++ home-manager-func ./home-desktop.nix);
+            msi2 = nixos-func ([ ./configuration-msi.nix ] ++ home-manager-func ./home-desktop.nix);
+            wsl = nixos-func (
+              [
+                ./configuration-wsl.nix
+                nixos-wsl.nixosModules.default
+              ]
+              ++ home-manager-func ./home-wsl.nix
+            );
+          };
+          homeConfigurations = {
+            "default" = home-manager.lib.homeManagerConfiguration {
+              pkgs = import nixpkgs { system = "x86_64-linux"; };
+              modules = [ ./home-wsl.nix ];
+            };
+          };
+        }
+      )
+      ({
+        nixosConfigurations = {
+          dada = conf;
+          msi2 = conf;
+        };
+      });
 }
