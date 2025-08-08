5ee0e1df7c97d5442afe7a1f39385934ef875589
DemyCode <verycols@gmail.com>
2025-08-08 13:45:08 +0200

    feat: adding tor

--- home-desktop.nix
+++ home-desktop.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 {
   imports = [ ./home.nix ];
-  home.packages = with pkgs; [ librewolf ];
+  home.packages = with pkgs; [
+    librewolf
+    tor-browser
+  ];
   home.file = {
     "./.config/kitty/kitty.conf" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/kitty/kitty.conf";
