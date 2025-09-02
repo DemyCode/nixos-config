3249dd5b31c487bbbde60e80418c9071e21948c4
DemyCode <verycols@gmail.com>
2025-09-02 20:49:36 +0200

    feat: adding cloudflared

--- home-jovyan.nix
+++ home-jovyan.nix
@@ -2,6 +2,9 @@
 {
   home.username = "jovyan";
   home.homeDirectory = "/home/jovyan";
+  home.packages = with pkgs; [
+    cloudflared
+  ];
   imports = [ ./home-terminal.nix ];
   home.sessionVariables.PATH = "$HOME/.nix-profile/bin:$PATH";
 }
