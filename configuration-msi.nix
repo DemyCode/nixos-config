b60364e4ea7638d814b7a30f50940f08dc2181e2
DemyCode <verycols@gmail.com>
2025-12-21 23:37:03 +0100

    feat: update configuration-msi.nix and flake.lock

--- configuration-msi.nix
+++ configuration-msi.nix
@@ -97,7 +97,29 @@
   };
   programs.fish.enable = true;
   environment.systemPackages = with pkgs; [
+    # KDE
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kclock # Clock app
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional: Manage the disk devices, partitions and file systems on your computer
+    # Non-KDE graphical packages
+    hardinfo2 # System information and benchmarks for Linux systems
+    vlc # Cross-platform media player and streaming server
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
     linuxKernel.packages.linux_zen.xone
+    wl-clipboard # Command-line copy/paste utilities for Wayland
+    # Non-KDE graphical packages
+    hardinfo2 # System information and benchmarks for Linux systems
+    vlc # Cross-platform media player and streaming server
+    wayland-utils # Wayland utilities
     signal-desktop
     spotify
     qbittorrent
@@ -105,7 +127,7 @@
     kitty
     discord
     inputs.nix-software-center.packages.${system}.nix-software-center
-    vlc
+    # vlc
     monero-gui
     qemu
     quickemu
@@ -115,6 +137,17 @@
       package = pkgs.mullvad-vpn;
     })
   ];
+  # environment.plasma6.excludePackages = with pkgs; [
+  #   kdePackages.kate
+  #   kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+  #   kdePackages.kcalc # Calculator
+  #   kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+  #   kdePackages.kclock # Clock app
+  #   kdePackages.kcolorchooser # A small utility to select a color
+  #   kdePackages.kolourpaint # Easy-to-use paint program
+  #   kdePackages.ksystemlog # KDE SystemLog Application
+  #   kdePackages.sddm-kcm # Configuration module for SDDM
+  # ];
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
   networking.firewall.allowedTCPPorts = [ 57621 ];
