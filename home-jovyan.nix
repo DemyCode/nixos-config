101b93282f70ec33fa73fddbe7779aae8c89a92d
MehdiBekhtaoui <mehdi.bekhtaoui@physicsx.ai>
2026-01-09 10:03:16 +0100

    feat: git link + removed cloudflared ngroked

--- home-jovyan.nix
+++ home-jovyan.nix
@@ -2,10 +2,6 @@
 {
   home.username = "jovyan";
   home.homeDirectory = "/home/jovyan";
-  home.packages = with pkgs; [
-    cloudflared
-    ngrok
-  ];
   imports = [ ./home-terminal.nix ];
   home.sessionVariables.PATH = "$HOME/.nix-profile/bin:$PATH";
 }
