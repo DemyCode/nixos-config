5c1a175e769c2ee0f562e5c7d6bb6c67df936cc8
DemyCode <verycols@gmail.com>
2026-01-18 01:04:22 +0100

    feat: program adb removed

--- configuration-msi.nix
+++ configuration-msi.nix
@@ -171,7 +171,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-  programs.adb.enable = true;
+  # programs.adb.enable = true;
   users.users.nixos.extraGroups = [
     "adbusers"
     "kvm"
