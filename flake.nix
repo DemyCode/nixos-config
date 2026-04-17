3eea6e9c718eaad59b9f8894acc64d22dd312ffe
DemyCode <verycols@gmail.com>
2026-04-17 18:34:58 +0200

    Revert "feat: lets go"
    This reverts commit 29812cc2f028002e56f35b02238fb9e405986a02.

--- flake.nix
+++ flake.nix
@@ -1,98 +1,116 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-software-center.url = "github:snowfallorg/nix-software-center";
-    nix-darwin = {
-      url = "github:nix-darwin/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+  inputs.nixos-wsl.url = "github:nix-community/NixOS-WSL";
+  inputs.nix-index-database.url = "github:nix-community/nix-index-database";
+  inputs.nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nix-software-center.url = "github:snowfallorg/nix-software-center";
+  # Use `github:nix-darwin/nix-darwin/nix-darwin-25.05` to use Nixpkgs 25.05.
+  inputs.nix-darwin.url = "github:nix-darwin/nix-darwin";
+  inputs.nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  # inputs.mac-app-util.url = "github:hraban/mac-app-util";
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-
-      flake =
-        let
-          nixos =
-            modules:
-            inputs.nixpkgs.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = modules;
-              specialArgs = { inherit inputs; };
-            };
-          withHomeManager = home-manager-module: [
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.nixos = home-manager-module;
-            }
-            inputs.nix-index-database.nixosModules.nix-index
-          ];
-          mkHome =
-            system: modules:
-            inputs.home-manager.lib.homeManagerConfiguration {
-              pkgs = import inputs.nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-              };
-              modules = modules;
-            };
-        in
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-wsl,
+      nix-index-database,
+      nix-darwin,
+      # mac-app-util,
+      ...
+    }@inputs:
+    let
+      nixos-func =
+        modules:
+        (nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = modules;
+          specialArgs = { inherit inputs; };
+        });
+      home-manager-func = home-manager-module: [
+        home-manager.nixosModules.home-manager
         {
-          nixosConfigurations = {
-            asus = nixos ([ ./configuration-asus.nix ] ++ withHomeManager ./home-desktop.nix);
-            msi = nixos ([ ./configuration-msi.nix ] ++ withHomeManager ./home-desktop.nix);
-            wsl = nixos (
-              [
-                ./configuration-wsl.nix
-                inputs.nixos-wsl.nixosModules.default
-              ]
-              ++ withHomeManager ./home-wsl.nix
-            );
-          };
-
-          homeConfigurations = {
-            "nixos" = mkHome "x86_64-linux" [ ./home-wsl.nix ];
-            "jovyan" = mkHome "x86_64-linux" [ ./home-jovyan.nix ];
-            "mehdibekhtaoui" = mkHome "x86_64-linux" [ ./home-mehdibekhtaoui.nix ];
-          };
-
-          darwinConfigurations."MacBook-Pro" = inputs.nix-darwin.lib.darwinSystem {
-            system = "aarch64-darwin";
-            pkgs = import inputs.nixpkgs {
-              system = "aarch64-darwin";
-              config = {
-                allowUnfree = true;
-                allowBroken = true;
-              };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos = home-manager-module;
+        }
+        nix-index-database.nixosModules.nix-index
+      ];
+      # Use this for all other targets
+      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
+    in
+    {
+      nixosConfigurations = {
+        asus = nixos-func ([ ./configuration-asus.ix ] ++ home-manager-func ./home-desktop.nix);
+        msi = nixos-func ([ ./configuration-msi.nix ] ++ home-manager-func ./home-desktop.nix);
+        wsl = nixos-func (
+          [
+            ./configuration-wsl.nix
+            nixos-wsl.nixosModules.default
+          ]
+          ++ home-manager-func ./home-wsl.nix
+        );
+      };
+      homeConfigurations = {
+        "nixos" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
             };
-            specialArgs = { inherit inputs; };
-            modules = [
-              ./configuration-darwin.nix
-              inputs.home-manager.darwinModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.admPX-MQ4LQGK4QM = import ./home-darwin.nix;
-              }
-            ];
+          };
+          modules = [ ./home-wsl.nix ];
+        };
+        "jovyan" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
+            };
+          };
+          modules = [ ./home-jovyan.nix ];
+        };
+        "mehdibekhtaoui" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
+            };
+          };
+          modules = [ ./home-mehdibekhtaoui.nix ];
+        };
+      };
+
+      darwinConfigurations."MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
           };
         };
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration-darwin.nix
+          # mac-app-util.darwinModules.default
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.admPX-MQ4LQGK4QM = import ./home-darwin.nix;
+            home-manager.sharedModules = [
+              # mac-app-util.homeManagerModules.default
+            ];
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
     };
 }
